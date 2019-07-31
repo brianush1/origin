1c6c012f9cad66aba93c34c17a4ad7b7b6659357
User Last <brianush1@outlook.com>
2019-07-31 19:58:46 +0300

    add odef decoding

--- Origin/odef.cpp
+++ Origin/odef.cpp
@@ -83,7 +83,107 @@ namespace origin {
 		ostr << final;
 	}
 
-	program* decode(std::istream& str, allocator& alloc) {
+	static inline uint8_t read_uint8(std::istream& str) {
+		return str.get();
+	}
 
+	static inline uint16_t read_uint16(std::istream& str) {
+		auto a = read_uint8(str);
+		auto b = read_uint8(str);
+		return a | (b << 8);
+	}
+
+	static inline uint32_t read_uint32(std::istream& str) {
+		auto a = read_uint16(str);
+		auto b = read_uint16(str);
+		return a | (b << 16);
+	}
+
+	static inline uint64_t read_uint64(std::istream& str) {
+		auto a = read_uint32(str);
+		auto b = read_uint32(str);
+		return a | (b << 32);
+	}
+
+	static inline std::string read_str(std::istream& str) {
+		uint64_t length = read_uint64(str);
+		char data[length + 1];
+		str.read(data, length);
+		data[length] = 0;
+		return data;
+	}
+
+	static inline typing* decode_typing(std::istream& str, allocator& memory) {
+		auto result = memory.allocate<typing>();
+		result->alias_name = result->name = read_str(str);
+		auto size = read_uint64(str);
+		for (uint64_t i = 0; i < size; ++i) {
+			result->templates.push_back(decode_typing(str, memory));
+		}
+		return result;
+	}
+
+	static inline vardecl* decode_vardecl(std::istream& str, allocator& memory) {
+		auto result = memory.allocate<vardecl>();
+		result->variable = read_str(str);
+		if (read_uint8(str) == 1) {
+			result->type = decode_typing(str, memory);
+		}
+		else {
+			result->type = nullptr;
+		}
+		result->init_value = nullptr;
+		return result;
+	}
+
+	program* decode(std::istream& str, allocator& memory) {
+		str.seekg(0, std::ios_base::end);
+		uint64_t length = str.tellg();
+		str.seekg(0, std::ios_base::beg);
+		if (length < 12) return nullptr;
+		char sign[5];
+		str.read(sign, 4);
+		sign[4] = 0;
+		std::ostringstream rsign;
+		rsign << "Org" << (char)0x92;
+		if (sign != rsign.str()) return nullptr;
+		read_uint64(str);
+		auto result = memory.allocate<program>();
+		result->namespace_name = read_str(str);
+		for (uint64_t size = read_uint64(str), i = 0; i < size; ++i) {
+			auto import = memory.allocate<variable>();
+			import->name = read_str(str);
+			result->imports.push_back(import);
+		}
+		for (uint64_t size = read_uint64(str), i = 0; i < size; ++i) {
+			result->vardecls.push_back(decode_vardecl(str, memory));
+		}
+		for (uint64_t size = read_uint64(str), i = 0; i < size; ++i) {
+			auto def = memory.allocate<classdef>();
+			def->name = read_str(str);
+			def->is_struct = read_uint8(str) == 1;
+			def->variadic = read_uint8(str) == 1;
+			for (uint64_t size = read_uint64(str), i = 0; i < size; ++i) {
+				def->generics.push_back(read_str(str));
+			}
+			for (uint64_t size = read_uint64(str), i = 0; i < size; ++i) {
+				def->vardecls.push_back(decode_vardecl(str, memory));
+				switch (read_uint8(str)) {
+				case 0:
+					def->accesses.push_back(access::private_access);
+					break;
+				case 1:
+					def->accesses.push_back(access::public_access);
+					break;
+				}
+			}
+			result->classes.push_back(def);
+		}
+		for (uint64_t size = read_uint64(str), i = 0; i < size; ++i) {
+			auto key = read_str(str);
+			auto value = decode_typing(str, memory);
+			result->aliases[key] = value;
+		}
+		return result;
 	}
 }
